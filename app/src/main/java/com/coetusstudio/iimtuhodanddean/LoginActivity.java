1c296e6494f77d798638b0550693062e4979a4bf
Nawaz <nzan576@gmail.com>
2022-04-06 03:51:59 +0530

    Initial commit

---
+++ app/src/main/java/com/coetusstudio/iimtuhodanddean/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.coetusstudio.iimtuhodanddean;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/coetusstudio/iimtuhodanddean/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.coetusstudio.iimtuhodanddean;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
